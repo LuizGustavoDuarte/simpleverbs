399bfc0f242079bfc8e042f6f29698ff3fc4fa21
LuizGustavoDuarte <elfzero0@gmail.com>
2023-03-02 13:14:18 -0300

    adicionando tabela inicial

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,50 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import { render } from 'react-dom'
 
 function App() {
-  const [lista, setLista] = useState([{verbo: 'fall', simplePast: 'fell'}, {verbo: 'play', simplePast: 'played'}, {verbo: 'write', simplePast: 'wrote'}])
+  const [lista, setLista] = useState([{verbo: 'fall', simplePast: 'fell', traducao: 'cair'}, {verbo: 'play', simplePast: 'played', traducao: "jogar"}, {verbo: 'write', simplePast: 'wrote', traducao: 'escrever'}])
 
   return (
     <div className="App">
-      <table style={{padding: "20px"}}>
-        <th style={{padding: "20px"}}>Verbo</th>
-        <th>Simple Past</th>
-        <tr>
-          <td>Run</td>
-          <td><input type="text" name="" id="" style={{border: "none", backgroundColor: '#002', textAlign: 'center'}}/></td>
-        </tr>
-        <tr>
-          <td><input type="text" name="" id="" style={{border: "none", backgroundColor: '#002', textAlign: 'center'}}/></td>
-          <td>Ran</td>
-        </tr>
-        <tr>
-          <td><input type="text" name="" id="" style={{border: "none", backgroundColor: '#002', textAlign: 'center'}}/></td>
-          <td><input type="text" name="" id="" style={{border: "none", backgroundColor: '#002', textAlign: 'center'}}/></td>
-        </tr>
-        <tr>
-          <td>Run</td>
-          <td>Ran</td>
-        </tr>
-        <tr>
-          <td>Run</td>
-          <td>Ran</td>
-        </tr>
-        <tr>
-          <td>Run</td>
-          <td>Ran</td>
-        </tr>
-      </table>
+     
+      
+        
+        <div className='container'>
+          <div className="row">
+            <div className='col-3 text-center'>#</div>
+            <div className='col-3 text-center'>Verbo</div>
+            <div className='col-3 text-center'>Simple Past</div>
+            <div className='col-3 text-center'>Tradução</div>
+          </div>
+        
+        
+       
 
-      <button style={{background: 'green'}}>Aleatorizar</button>
+        {lista.map((verbo, key) => {
+          
+          let inputStyle = {border: "none", backgroundColor: '#95baf5', textAlign: 'center', width: '10vw', marginInline: 10, marginTop: '5px', marginBottom: '5px', color: 'white'}
+
+          return(
+
+           
+              <div className="row" key={key}>
+                <div className='col-3'>#{key+1}</div>
+                {Math.floor(Math.random() * 10) > 5 ? <div className='col-3 text-center'>{verbo.verbo}</div> : <div className='col-3 text-center'><input type="text" name="" id="" style={inputStyle}/></div>}
+                {Math.floor(Math.random() * 10) > 5 ? <div className='col-3 text-center'>{verbo.simplePast}</div> : <div className='col-3 text-center'><input type="text" name="" id="" style={inputStyle}/></div>}
+                {Math.floor(Math.random() * 10) > 5 ? <div className='col-3 text-center'>{verbo.traducao}</div> : <div className='col-3 text-center'><input type="text" name="" id="" style={inputStyle}/></div>}
+              
+              </div>
+            
+
+
+            
+          )
+        })}
+      </div>
+      
+     
     </div>
   )
 }
